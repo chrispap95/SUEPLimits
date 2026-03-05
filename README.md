93b64f220c7109f7fc633517e418ba0cd228bd18
Christos Papageorgakis <chrispap95@gmail.com>
2026-03-05 14:36:34 -0600

    Update version and instructions

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Limits for the Muon Counting SUEP analysis
 
-Will describe the code here later.
+## Initial setup
+
+To setup the area, do the following:
+```bash
+wget https://raw.githubusercontent.com/chrispap95/SUEPLimits/refs/heads/main/setup.sh
+./setup.sh
+cd CMSSW_14_1_0_pre4/src
+cmsenv
+```
+
+All you need is in the `run_*.sh` scripts (commands to run, examples, etc.).
--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ cd $CMSSW_VERSION/src
 cmsenv
 
 # Clone Combine
-git -c advice.detachedHead=false clone --depth 1 --branch v10.4.1 https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
+git -c advice.detachedHead=false clone --depth 1 --branch v10.5.1 https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 
 # Clone CombineHarvester
-git clone --branch v3.0.0 https://github.com/cms-analysis/CombineHarvester.git CombineHarvester
+git clone --branch v3.1.0 https://github.com/cms-analysis/CombineHarvester.git CombineHarvester
 
 # Clone SUEPLimits
 git clone git@github.com:chrispap95/SUEPLimits.git SUEPLimits/SUEPLimits
